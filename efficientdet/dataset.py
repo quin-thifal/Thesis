75775a77d118980570413c12d9411651f4f248c1
Quin <thifal.quinolina@gmail.com>
2024-08-02 13:25:04 +0700

    past checkpoint

--- efficientdet/dataset.py
+++ efficientdet/dataset.py
@@ -1,12 +1,15 @@
 import os
-import numpy as np
-import cv2
 import torch
+import numpy as np
+
 from torch.utils.data import Dataset, DataLoader
 from pycocotools.coco import COCO
+import cv2
+
 
 class CocoDataset(Dataset):
     def __init__(self, root_dir, set='train2017', transform=None):
+
         self.root_dir = root_dir
         self.set_name = set
         self.transform = transform
@@ -17,6 +20,8 @@ class CocoDataset(Dataset):
         self.load_classes()
 
     def load_classes(self):
+
+        # load class names (name -> label)
         categories = self.coco.loadCats(self.coco.getCatIds())
         categories.sort(key=lambda x: x['id'])
 
@@ -24,20 +29,19 @@ class CocoDataset(Dataset):
         for c in categories:
             self.classes[c['name']] = len(self.classes)
 
-        self.labels = {value: key for key, value in self.classes.items()}
+        # also load the reverse (label -> name)
+        self.labels = {}
+        for key, value in self.classes.items():
+            self.labels[value] = key
 
     def __len__(self):
         return len(self.image_ids)
 
     def __getitem__(self, idx):
+
         img = self.load_image(idx)
         annot = self.load_annotations(idx)
-        image_info = self.coco.loadImgs(self.image_ids[idx])[0]
-        filename = image_info['file_name']
-
-        scale = 1.0  # Placeholder for scale
-
-        sample = {'img': img, 'annot': annot, 'filename': filename, 'scale': scale}
+        sample = {'img': img, 'annot': annot}
         if self.transform:
             sample = self.transform(sample)
         return sample
@@ -51,14 +55,19 @@ class CocoDataset(Dataset):
         return img.astype(np.float32) / 255.
 
     def load_annotations(self, image_index):
+        # get ground truth annotations
         annotations_ids = self.coco.getAnnIds(imgIds=self.image_ids[image_index], iscrowd=False)
         annotations = np.zeros((0, 5))
 
+        # some images appear to miss annotations
         if len(annotations_ids) == 0:
             return annotations
 
+        # parse annotations
         coco_annotations = self.coco.loadAnns(annotations_ids)
         for idx, a in enumerate(coco_annotations):
+
+            # some annotations have basically no width / height, skip them
             if a['bbox'][2] < 1 or a['bbox'][3] < 1:
                 continue
 
@@ -67,35 +76,40 @@ class CocoDataset(Dataset):
             annotation[0, 4] = a['category_id'] - 1
             annotations = np.append(annotations, annotation, axis=0)
 
+        # transform from [x, y, w, h] to [x1, y1, x2, y2]
         annotations[:, 2] = annotations[:, 0] + annotations[:, 2]
         annotations[:, 3] = annotations[:, 1] + annotations[:, 3]
 
         return annotations
 
-    def collate_fn(self, data):
-        imgs = [s['img'] for s in data]
-        annots = [s['annot'] for s in data]
-        filenames = [s['filename'] for s in data]
-        scales = [s['scale'] for s in data]
 
-        imgs = torch.from_numpy(np.stack(imgs, axis=0))
+def collater(data):
+    imgs = [s['img'] for s in data]
+    annots = [s['annot'] for s in data]
+    scales = [s['scale'] for s in data]
 
-        max_num_annots = max(annot.shape[0] for annot in annots)
+    imgs = torch.from_numpy(np.stack(imgs, axis=0))
 
-        if max_num_annots > 0:
-            annot_padded = torch.ones((len(annots), max_num_annots, 5)) * -1
-            for idx, annot in enumerate(annots):
-                if annot.shape[0] > 0:
-                    annot_padded[idx, :annot.shape[0], :] = annot
-        else:
-            annot_padded = torch.ones((len(annots), 1, 5)) * -1
+    max_num_annots = max(annot.shape[0] for annot in annots)
 
-        imgs = imgs.permute(0, 3, 1, 2)
+    if max_num_annots > 0:
 
-        return {'img': imgs, 'annot': annot_padded, 'filename': filenames, 'scale': scales}
+        annot_padded = torch.ones((len(annots), max_num_annots, 5)) * -1
+
+        for idx, annot in enumerate(annots):
+            if annot.shape[0] > 0:
+                annot_padded[idx, :annot.shape[0], :] = annot
+    else:
+        annot_padded = torch.ones((len(annots), 1, 5)) * -1
+
+    imgs = imgs.permute(0, 3, 1, 2)
+
+    return {'img': imgs, 'annot': annot_padded, 'scale': scales}
 
 
 class Resizer(object):
+    """Convert ndarrays in sample to Tensors."""
+    
     def __init__(self, img_size=512):
         self.img_size = img_size
 
@@ -122,6 +136,8 @@ class Resizer(object):
 
 
 class Augmenter(object):
+    """Convert ndarrays in sample to Tensors."""
+
     def __call__(self, sample, flip_x=0.5):
         if np.random.rand() < flip_x:
             image, annots = sample['img'], sample['annot']
@@ -143,11 +159,12 @@ class Augmenter(object):
 
 
 class Normalizer(object):
+
     def __init__(self, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-        self.mean = np.array(mean, dtype=np.float32)
-        self.std = np.array(std, dtype=np.float32)
+        self.mean = np.array([[mean]])
+        self.std = np.array([[std]])
 
     def __call__(self, sample):
         image, annots = sample['img'], sample['annot']
-        image = (image.astype(np.float32) - self.mean) / self.std
-        return {'img': torch.from_numpy(image), 'annot': annots}
+
+        return {'img': ((image.astype(np.float32) - self.mean) / self.std), 'annot': annots}
--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from torchvision import transforms
 from tqdm.autonotebook import tqdm
 
 from backbone import EfficientDetBackbone
-from efficientdet.dataset import CocoDataset, Resizer, Normalizer, Augmenter
+from efficientdet.dataset import CocoDataset, Resizer, Normalizer, Augmenter, collater
 from efficientdet.loss import FocalLoss
 from utils.sync_batchnorm import patch_replication_callback
 from utils.utils import replace_w_sync_bn, CustomDataParallel, get_last_weights, init_weights, boolean_string
@@ -38,10 +38,10 @@ def get_args():
                              'useful in early stage convergence or small/easy dataset')
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--optim', type=str, default='adamw', help='select optimizer for training, '
-                                                                   'suggest using \'adamw\' until the'
+                                                                   'suggest using \'admaw\' until the'
                                                                    ' very final stage then switch to \'sgd\'')
     parser.add_argument('--num_epochs', type=int, default=500)
-    parser.add_argument('--val_interval', type=int, default=1, help='Number of epochs between validation phases')
+    parser.add_argument('--val_interval', type=int, default=1, help='Number of epoches between valing phases')
     parser.add_argument('--save_interval', type=int, default=500, help='Number of steps between saving')
     parser.add_argument('--es_min_delta', type=float, default=0.0,
                         help='Early stopping\'s parameter: minimum change loss to qualify as an improvement')
@@ -55,37 +55,9 @@ def get_args():
     parser.add_argument('--debug', type=boolean_string, default=False,
                         help='whether visualize the predicted boxes of training, '
                              'the output images will be in test/')
-    parser.add_argument('--threshold', type=float, default=0.05,
-                        help='Threshold for filtering predictions during validation')
 
     args = parser.parse_args()
     return args
-
-
-def postprocess(imgs, anchors, regression, classification, regressBoxes, clipBoxes, threshold, nms_threshold,
-                mask=None):
-    classification = torch.sigmoid(classification)
-    regression = torch.sigmoid(regression)
-
-    if mask is not None:
-        classification = classification * mask
-
-    preds = []
-    for i in range(len(imgs)):
-        pred_boxes = []
-        # Example: Replace this with actual bbox processing logic
-        # Apply threshold to classification scores
-        scores = classification[i].max(dim=1)[0]
-        keep = scores > threshold
-
-        # Implement NMS if needed here
-
-        pred_boxes.append({
-            'boxes': [],  # Fill with processed boxes
-            'scores': scores[keep]  # Filtered scores
-        })
-
-    return preds
 
 
 class ModelWithLoss(nn.Module):
@@ -105,18 +77,6 @@ class ModelWithLoss(nn.Module):
         return cls_loss, reg_loss
 
 
-def collate_fn(batch):
-    imgs = [item['img'] for item in batch]
-    annot = [item['annot'] for item in batch]
-    filenames = [item['filename'] for item in batch]
-    scales = [item['scale'] for item in batch]
-
-    imgs = torch.stack(imgs, 0)
-    annot = torch.stack(annot, 0)
-
-    return {'img': imgs, 'annot': annot, 'filename': filenames, 'scale': scales}
-
-
 def train(opt):
     params = Params(f'project/{opt.project}.yml')
 
@@ -133,35 +93,34 @@ def train(opt):
     os.makedirs(opt.log_path, exist_ok=True)
     os.makedirs(opt.saved_path, exist_ok=True)
 
-    input_sizes = [512, 640, 768, 896, 1024]  # Example sizes for different coefficients
-
-    train_dataset = CocoDataset(root_dir=os.path.join(opt.data_path, params.project_name), set=params.train_set,
-                                transform=transforms.Compose([Normalizer(mean=params.mean, std=params.std),
-                                                              Augmenter(),
-                                                              Resizer(input_sizes[opt.compound_coef])]))
-
-    val_dataset = CocoDataset(root_dir=os.path.join(opt.data_path, params.project_name), set=params.val_set,
-                              transform=transforms.Compose([Normalizer(mean=params.mean, std=params.std),
-                                                            Resizer(input_sizes[opt.compound_coef])]))
-
     training_params = {'batch_size': opt.batch_size,
                        'shuffle': True,
                        'drop_last': True,
-                       'collate_fn': collate_fn,
+                       'collate_fn': collater,
                        'num_workers': opt.num_workers}
 
     val_params = {'batch_size': opt.batch_size,
                   'shuffle': False,
                   'drop_last': True,
-                  'collate_fn': collate_fn,
+                  'collate_fn': collater,
                   'num_workers': opt.num_workers}
 
-    training_generator = DataLoader(train_dataset, **training_params)
-    val_generator = DataLoader(val_dataset, **val_params)
+    input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536, 1536]
+    training_set = CocoDataset(root_dir=os.path.join(opt.data_path, params.project_name), set=params.train_set,
+                               transform=transforms.Compose([Normalizer(mean=params.mean, std=params.std),
+                                                             Augmenter(),
+                                                             Resizer(input_sizes[opt.compound_coef])]))
+    training_generator = DataLoader(training_set, **training_params)
+
+    val_set = CocoDataset(root_dir=os.path.join(opt.data_path, params.project_name), set=params.val_set,
+                          transform=transforms.Compose([Normalizer(mean=params.mean, std=params.std),
+                                                        Resizer(input_sizes[opt.compound_coef])]))
+    val_generator = DataLoader(val_set, **val_params)
 
     model = EfficientDetBackbone(num_classes=len(params.obj_list), compound_coef=opt.compound_coef,
                                  ratios=eval(params.anchors_ratios), scales=eval(params.anchors_scales))
 
+    # load last weights
     if opt.load_weights is not None:
         if opt.load_weights.endswith('.pth'):
             weights_path = opt.load_weights
@@ -173,7 +132,7 @@ def train(opt):
             last_step = 0
 
         try:
-            model.load_state_dict(torch.load(weights_path), strict=False)
+            ret = model.load_state_dict(torch.load(weights_path), strict=False)
         except RuntimeError as e:
             print(f'[Warning] Ignoring {e}')
             print(
@@ -185,6 +144,7 @@ def train(opt):
         print('[Info] initializing weights...')
         init_weights(model)
 
+    # freeze backbone if train head_only
     if opt.head_only:
         def freeze_backbone(m):
             classname = m.__class__.__name__
@@ -196,6 +156,13 @@ def train(opt):
         model.apply(freeze_backbone)
         print('[Info] freezed backbone')
 
+    # https://github.com/vacancy/Synchronized-BatchNorm-PyTorch
+    # apply sync_bn when using multiple gpu and batch_size per gpu is lower than 4
+    #  useful when gpu memory is limited.
+    # because when bn is disable, the training will be very unstable or slow to converge,
+    # apply sync_bn can solve it,
+    # by packing all mini-batch across all gpus as one batch and normalize, then send it back to all gpus.
+    # but it would also slow down the training by a little bit.
     if params.num_gpus > 1 and opt.batch_size // params.num_gpus < 4:
         model.apply(replace_w_sync_bn)
         use_sync_bn = True
@@ -204,6 +171,7 @@ def train(opt):
 
     writer = SummaryWriter(opt.log_path + f'/{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}/')
 
+    # warp the model with loss function, to reduce the memory usage on gpu0 and speedup
     model = ModelWithLoss(model, debug=opt.debug)
 
     if params.num_gpus > 0:
@@ -215,50 +183,140 @@ def train(opt):
 
     if opt.optim == 'adamw':
         optimizer = torch.optim.AdamW(model.parameters(), opt.lr)
-    elif opt.optim == 'sgd':
-        optimizer = torch.optim.SGD(model.parameters(), opt.lr, momentum=0.9, weight_decay=5e-4)
     else:
-        raise ValueError('Optimizer must be adamw or sgd')
+        optimizer = torch.optim.SGD(model.parameters(), opt.lr, momentum=0.9, nesterov=True)
 
-    for epoch in range(last_step // len(training_generator) + 1, opt.num_epochs + 1):
-        model.train()
-        for step, (imgs, annotations, _) in tqdm(enumerate(training_generator)):
-            if torch.cuda.is_available():
-                imgs = imgs.cuda()
-                annotations = [ann.cuda() for ann in annotations]
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
 
-            optimizer.zero_grad()
-            cls_loss, reg_loss = model(imgs, annotations)
-            loss = cls_loss + reg_loss
-            loss.backward()
-            optimizer.step()
+    epoch = 0
+    best_loss = 1e5
+    best_epoch = 0
+    step = max(0, last_step)
+    model.train()
 
-            if step % 10 == 0:
-                writer.add_scalar('Train/Loss', loss.item(), epoch * len(training_generator) + step)
-                writer.add_scalar('Train/Cls_Loss', cls_loss.item(), epoch * len(training_generator) + step)
-                writer.add_scalar('Train/Reg_Loss', reg_loss.item(), epoch * len(training_generator) + step)
+    num_iter_per_epoch = len(training_generator)
 
-        if epoch % opt.val_interval == 0:
-            model.eval()
-            with torch.no_grad():
-                val_loss = 0
-                for imgs, annotations, _ in tqdm(val_generator):
-                    if torch.cuda.is_available():
+    try:
+        for epoch in range(opt.num_epochs):
+            last_epoch = step // num_iter_per_epoch
+            if epoch < last_epoch:
+                continue
+
+            epoch_loss = []
+            progress_bar = tqdm(training_generator)
+            for iter, data in enumerate(progress_bar):
+                if iter < step - last_epoch * num_iter_per_epoch:
+                    progress_bar.update()
+                    continue
+                try:
+                    imgs = data['img']
+                    annot = data['annot']
+
+                    if params.num_gpus == 1:
+                        # if only one gpu, just send it to cuda:0
+                        # elif multiple gpus, send it to multiple gpus in CustomDataParallel, not here
                         imgs = imgs.cuda()
-                        annotations = [ann.cuda() for ann in annotations]
-                    cls_loss, reg_loss = model(imgs, annotations)
+                        annot = annot.cuda()
+
+                    optimizer.zero_grad()
+                    cls_loss, reg_loss = model(imgs, annot, obj_list=params.obj_list)
+                    cls_loss = cls_loss.mean()
+                    reg_loss = reg_loss.mean()
+
                     loss = cls_loss + reg_loss
-                    val_loss += loss.item()
+                    if loss == 0 or not torch.isfinite(loss):
+                        continue
 
-                writer.add_scalar('Val/Loss', val_loss / len(val_generator), epoch)
+                    loss.backward()
+                    # torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+                    optimizer.step()
 
-        if epoch % opt.save_interval == 0:
-            save_path = os.path.join(opt.saved_path, f'epoch_{epoch}.pth')
-            torch.save(model.state_dict(), save_path)
+                    epoch_loss.append(float(loss))
 
+                    progress_bar.set_description(
+                        'Step: {}   ||  Tra Epoch: {}/{}    ||  Itr: {}/{}  ||  Cls Loss: {:.5f}    ||  Reg Loss: {:.5f}    ||  Ttl Loss: {:.5f}'.format(
+                            step, epoch, opt.num_epochs, iter + 1, num_iter_per_epoch, cls_loss.item(),
+                            reg_loss.item(), loss.item()))
+                    writer.add_scalars('Loss', {'train': loss}, step)
+                    writer.add_scalars('Reg Loss', {'train': reg_loss}, step)
+                    writer.add_scalars('Cls Loss', {'train': cls_loss}, step)
+
+                    # log learning_rate
+                    current_lr = optimizer.param_groups[0]['lr']
+                    writer.add_scalar('LR', current_lr, step)
+
+                    step += 1
+
+                    if step % opt.save_interval == 0 and step > 0:
+                        save_checkpoint(model, f'efficientdet-d{opt.compound_coef}_{epoch}_{step}.pth')
+                        print('checkpoint...')
+
+                except Exception as e:
+                    print('[Error]', traceback.format_exc())
+                    print(e)
+                    continue
+            scheduler.step(np.mean(epoch_loss))
+
+            if epoch % opt.val_interval == 0:
+                model.eval()
+                loss_regression_ls = []
+                loss_classification_ls = []
+                for iter, data in enumerate(val_generator):
+                    with torch.no_grad():
+                        imgs = data['img']
+                        annot = data['annot']
+
+                        if params.num_gpus == 1:
+                            imgs = imgs.cuda()
+                            annot = annot.cuda()
+
+                        cls_loss, reg_loss = model(imgs, annot, obj_list=params.obj_list)
+                        cls_loss = cls_loss.mean()
+                        reg_loss = reg_loss.mean()
+
+                        loss = cls_loss + reg_loss
+                        if loss == 0 or not torch.isfinite(loss):
+                            continue
+
+                        loss_classification_ls.append(cls_loss.item())
+                        loss_regression_ls.append(reg_loss.item())
+
+                cls_loss = np.mean(loss_classification_ls)
+                reg_loss = np.mean(loss_regression_ls)
+                loss = cls_loss + reg_loss
+
+                print(
+                    '               Val Epoch: {}/{}                   ||  Cls Loss: {:1.5f}   ||  Reg Loss: {:1.5f}   ||  Ttl Loss: {:1.5f}'.format(
+                        epoch, opt.num_epochs, cls_loss, reg_loss, loss))
+                writer.add_scalars('Loss', {'val': loss}, step)
+                writer.add_scalars('Reg Loss', {'val': reg_loss}, step)
+                writer.add_scalars('Cls Loss', {'val': cls_loss}, step)
+
+                if loss + opt.es_min_delta < best_loss:
+                    best_loss = loss
+                    best_epoch = epoch
+
+                    save_checkpoint(model, f'efficientdet-d{opt.compound_coef}_{epoch}_{step}.pth')
+
+                model.train()
+
+                # Early stopping
+                if epoch - best_epoch > opt.es_patience > 0:
+                    print('[Info] Stop training at epoch {}. The lowest loss achieved is {}'.format(epoch, best_loss))
+                    break
+    except KeyboardInterrupt:
+        save_checkpoint(model, f'efficientdet-d{opt.compound_coef}_{epoch}_{step}.pth')
+        writer.close()
     writer.close()
 
 
-if __name__ == "__main__":
-    args = get_args()
-    train(args)
+def save_checkpoint(model, name):
+    if isinstance(model, CustomDataParallel):
+        torch.save(model.module.model.state_dict(), os.path.join(opt.saved_path, name))
+    else:
+        torch.save(model.model.state_dict(), os.path.join(opt.saved_path, name))
+
+
+if __name__ == '__main__':
+    opt = get_args()
+    train(opt)
